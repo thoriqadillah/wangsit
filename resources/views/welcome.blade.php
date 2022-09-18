4d95c2d70f265b5c9ff1e78839d752207d874ad3
thoriqadillah <thoriqadillah59@gmail.com>
2022-09-18 13:39:47 +0700

    add tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        @vite('resources/css/app.css')
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -57,6 +58,7 @@
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
                                     Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
                                 </div>
+                                <button class="px-5 py-2 border">Button</button>
                             </div>
                         </div>
 
