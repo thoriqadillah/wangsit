3ed936894ddd424a1d11526dc30c4eba042d7cc4
thoriqadillah <thoriqadillah59@gmail.com>
2022-09-25 12:49:44 +0700

    tambah auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\AuthService;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    protected AuthService $auth;
+
+    public function __construct(AuthService $auth) {
+        $this->auth = $auth;
+    }
+
+    public function login(Request $request) {
+        $credentials = $request->validate([
+            'nim' => ['required', 'min:15', 'max:15'],
+            'password' => ['required', 'min:8']
+        ]);
+
+        $loggedIn = $this->auth->login($credentials['nim'], $credentials['password']);
+        if ($loggedIn) {
+            $request->session()->regenerate();
+            return redirect('/');
+        }
+
+        return back()->withErrors([
+            'nim' => 'NIM atau password salah',
+            'password' => 'NIM atau password salah',
+        ]);
+    }
+}
--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -2,6 +2,32 @@
 
 namespace App\Services;
 
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Auth;
+
 class AuthService {
-  
+  public function login(string $nim, string $password): bool {
+    if (!$this->validNim($nim)) return false;
+    
+    return !Auth::attempt(['nim' => $nim, 'password' => $password]) 
+      ? $this->register($nim, $password)
+      : true;
+  }
+
+  private function register(string $nim, string $password): bool {
+    $newUser = ScrapperService::scrapUser($nim, $password);
+    if (count($newUser) !== 0) {
+      User::create($newUser);
+      return Auth::attempt(['nim' => $nim, 'password' => $password]);
+    }
+
+    return false;
+  }
+
+  private function validNim(string $nim): bool {
+    $NIM_SI = '515040';
+    return substr($nim, 2, 6) === $NIM_SI;
+  }
+
 }
