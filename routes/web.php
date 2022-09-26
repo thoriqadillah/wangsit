e5e3f606bd4546f756c5d4b3248f885325f6c7d7
thoriqadillah <thoriqadillah59@gmail.com>
2022-09-26 11:23:13 +0700

    tambah limiter untuk login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ExampleController;
+use App\Models\User;
+use App\Services\AuthService;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,8 +18,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/login', [AuthController::class, 'login'])
+    ->name('login')
+    ->middleware(['throttle:login']); //limit rate request -> search RouteServiceProvider
 
 Route::get('/', function () {
     return view('welcome');
