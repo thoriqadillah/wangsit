1a5dbfbc6a814e4c5357bb7f67382365d687a7ad
thoriqadillah <thoriqadillah59@gmail.com>
2022-09-27 17:29:25 +0700

    mindah nim ke env biar test gampang

--- tests/Feature/AuthServiceTest.php
+++ tests/Feature/AuthServiceTest.php
@@ -12,7 +12,7 @@ class AuthServiceTest extends TestCase
     public function test_login_attempt_should_return_false()
     {
         $auth = new AuthService();
-        $loggedIn = $auth->login('195150400111034', 'secret');
+        $loggedIn = $auth->login(env('NIM_SIAM'), 'secret');
         
         $this->assertFalse($loggedIn);
     }
@@ -20,8 +20,12 @@ class AuthServiceTest extends TestCase
     public function test_login_attempt_should_return_true()
     {
         $auth = new AuthService();
-        $loggedIn = $auth->login('195150400111034', env('PASSWORD_SIAM'));
-        
+        $loggedIn = $auth->login(env('NIM_SIAM'), env('PASSWORD_SIAM'));
+
         $this->assertTrue($loggedIn);
+        $this->assertDatabaseHas('users', [
+            'nim' => env('NIM_SIAM')
+        ]);
+        
     }
 }
